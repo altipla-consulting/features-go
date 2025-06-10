9df7161611e3cd4e3b74b249648a73c86a16c595
ErnestNeller <ernest.neller@altiplaconsulting.com>
2025-06-10 19:22:25 +0200

    Implement feature flags library.

---
+++ features_test.go
@@ -0,0 +1,59 @@
+package features
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func InitTestbed() {
+	isLocal = false
+	client = &features{
+		flags: []*flag{
+			{
+				Code:    "feature-1",
+				Tenants: []string{"tenant-1"},
+				Global:  true,
+			},
+			{
+				Code:    "feature-2",
+				Tenants: []string{"tenant-1"},
+				Global:  false,
+			},
+		},
+		lastTime: time.Now(),
+	}
+}
+
+func TestFalseClientNotConfigured(t *testing.T) {
+	InitTestbed()
+	client = nil
+	require.False(t, Flag(context.Background(), "feature-1"))
+}
+
+func TestTrueFlag(t *testing.T) {
+	InitTestbed()
+	require.True(t, Flag(context.Background(), "feature-1"))
+}
+
+func TestTrueFlagWithTenant(t *testing.T) {
+	InitTestbed()
+	require.True(t, Flag(context.Background(), "feature-1", WithTenant("tenant-1")))
+}
+
+func TestFalseFlag(t *testing.T) {
+	InitTestbed()
+	require.False(t, Flag(context.Background(), "feature-2"))
+}
+
+func TestFalseFlagWithTenant(t *testing.T) {
+	InitTestbed()
+	require.False(t, Flag(context.Background(), "feature-2", WithTenant("tenant-1")))
+}
+
+func TestFalseFlagWithFalseTenant(t *testing.T) {
+	InitTestbed()
+	require.False(t, Flag(context.Background(), "feature-1", WithTenant("tenant-3")))
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,18 @@
 module github.com/altipla-consulting/features-go
 
-go 1.22.4
+go 1.23.0
+
+toolchain go1.23.10
+
+require github.com/altipla-consulting/errors v1.5.1
+
+require github.com/altipla-consulting/env v0.3.0
+
+require golang.org/x/sync v0.15.0
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.10.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
